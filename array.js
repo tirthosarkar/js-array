524467f523e150e4c2b6e05b6bb4ed1116f6036b
tirthosarkar <tirthosarkar1@gmail.com>
2026-03-31 14:31:03 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,16 @@
+// const num1 = 12;
+// const num2 = 15;
+// const num3 = 22;
+// const num4 = 154;
+// const num5 = 4712;
+
+
+// const numbers = 1222223334445;
+
+// (), {},[]
+
+const numbers = [12, 22, 54, 65];
+const friends = ['abul', 'kabul', 'dabul', 'habul'];
+const passed = [true, false,];
+const , mixedArray = [12, 'abul, true, 45.55'];
+
