3410862a60b118f709545cbcc87dda5c68e17673
tirthosarkar <tirthosarkar1@gmail.com>
2026-04-07 09:57:55 +0600

    added shift-unshift.js

--- pop.js
+++ pop.js
@@ -1,3 +1,8 @@
 const friends = ['balam', 'kalam', 'salam', 'gelam', 'pailam' ];
 // friends.push('khailam');
 console.log(friends);
+
+friends.pop();
+console.log(friends);
+friends.pop();
+console.log(friends);
---
+++ shift-unshift.js
@@ -0,0 +1,7 @@
+const friends = ['balam', 'kalam', 'salam', 'gelam', 'pailam'];
+console.log(friends);
+
+friends.shift();
+console.log(friends);
+friends.unshift('gumailam');
+console.log(friends);
