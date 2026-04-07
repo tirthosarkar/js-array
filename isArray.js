d04607c05f082fe6e55c48a90d0be7b046e31b06
tirthosarkar <tirthosarkar1@gmail.com>
2026-04-07 10:39:20 +0600

    added isArray.js

--- isArray.js
+++ isArray.js
@@ -4,9 +4,9 @@ const nums = [];
 
 const food = 'ros o gol l a';
 
-const age = 15;
+constage = 15;
 
 console.log(Array.isArray(friends))
 console.log(Array.isArray(nums))
 console.log(Array.isArray(food))
-console.log(Array.isArray(age))
+// console.log(Array.isArray(age))
